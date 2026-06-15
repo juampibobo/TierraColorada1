7041dc11a522853249851f461488ff3be41c745e
NicolasAyala22 <127499448+juampibobo@users.noreply.github.com>
2026-06-15 18:41:55 -0300

    Update Principal.java

--- src/main/java/com/mycompany/tierracolorada1/vistas/Principal.java
+++ src/main/java/com/mycompany/tierracolorada1/vistas/Principal.java
@@ -39,7 +39,7 @@ public class Principal {
     ProcesoControlador procesoCtrl = new ProcesoControlador();
     AuditoriaControlador seguridadCtrl = new AuditoriaControlador();
 
-    System.out.println("    SISTEMA DE TRAZABILIDAD - TIERRA COLORADA     ");
+    System.out.println("    SISTEMA DE TRAZABILIDAD - tierra colorada     ");
     
 
         boolean loginExitoso = false;
